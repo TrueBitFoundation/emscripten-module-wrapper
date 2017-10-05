2c4b82cc00af02f3587d2b6fe4bef8d898aa1eb3
Sami ZzZ <mrsmkl@gmail.com>
2017-10-05 09:20:06 +0200

    recording calls to binary file

--- pre-run.js
+++ pre-run.js
@@ -18,8 +18,9 @@ var calls = []
 function makeStub(name, func) {
     return function () {
         console.log("Calling ", name, arguments)
+        startMemoryRecord()
         var res = func.apply(null, arguments)
-        calls.push({result: res, args:Array.from(arguments), name:name})
+        calls.push({result: res, args:Array.from(arguments), name:name, memory:memory_record})
         console.log("Result", res)
         return res
     }
@@ -44,4 +45,57 @@ console.log(JSON.stringify(env_globals))
 var fs = require("fs")
 
 fs.writeFileSync(source_file + ".globals.json", JSON.stringify(env_globals))
+
+// writing calls
+
+var arr = []
+
+function u8(x) {
+    arr.push(x & 0xff)
+}
+
+function u16(x) {
+    u8(x)
+    u8(x >> 8)
+}
+
+function u32(x) {
+    u16(x)
+    u16(x >> 16)
+}
+
+function u64(x) {
+    u32(x)
+    u32(x >> 32)
+}
+
+// Only 32 bit heaps, makes things easier (they fit into JS numbers)
+
+function outputCall(call) {
+// number of args, args
+// arg might be 64 bit?
+    u16(call.args.length)
+    call.args.forEach(u64)
+// also number of returns
+    if (typeof call.result != "undefined") {
+        u16(1)
+        u64(call.result)
+    }
+    else u16(0)
+// just setting the memory for now
+    var h8 = call.memory.heap8.filter(x => typeof x == "object")
+    u32(h8.length)
+    h8.forEach(x => { u32(x[0]); u8(x[1]) })
+    var h16 = call.memory.heap16.filter(x => typeof x == "object")
+    u32(h16.length)
+    h16.forEach(x => { u32(x[0]); u16(x[1]) })
+    var h32 = call.memory.heap32.filter(x => typeof x == "object")
+    u32(h32.length)
+    h32.forEach(x => { u32(x[0]); u32(x[1]) })
+}
+
+u32(calls.length)
+calls.forEach(outputCall)
+
+fs.writeFileSync(source_file + ".calls.bin", Buffer.from(arr))
 
