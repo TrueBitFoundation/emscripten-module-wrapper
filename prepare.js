49aa1fb155f142f022cf121a1a0eb7fde03b003e
Sami Mäkelä <mrsmkl@gmail.com>
2018-01-10 16:46:29 +0200

    added floating point support

--- prepare.js
+++ prepare.js
@@ -4,6 +4,7 @@ var argv = require('minimist')(process.argv.slice(2))
 var execFile = require('child_process').execFile
 var ipfsAPI = require('ipfs-api')
 
+const { spawn } = require('child_process');
 var dir = "/home/sami/emscripten-module-wrapper/"
 
 var host = "programming-progress.com"
@@ -33,7 +34,7 @@ function uploadIPFS(fname) {
 
 function exec(cmd, args, dr) {
     return new Promise(function (cont,err) {
-        console.log(exec, cmd, args, dr)
+        console.log("exec: ", cmd, args, dr)
         execFile(cmd, args, {cwd:dr || tmp_dir}, function (error, stdout, stderr) {
             if (stderr) console.error('error ', stderr, args)
             if (stdout) console.log('output ', stdout, args)
@@ -43,8 +44,41 @@ function exec(cmd, args, dr) {
     })
 }
 
+function spawnPromise(cmd, args, dr) {
+    return new Promise(function (cont,err) {
+        console.log("exec: ", cmd + args.join(" "), dr)
+        const p = spawn(cmd, args, {cwd:dr || tmp_dir})
+        
+        p.on('error', (err) => {
+            console.log('Failed to start subprocess.');
+            err(error)
+        });
+
+        p.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        p.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+
+        p.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+            cont()
+        });
+
+    })
+}
+
 function flatten(lst) {
     return [].concat.apply([],lst)
+}
+
+function clean(obj, field) {
+    var x = obj[field]
+    if (typeof x == "object") return
+    if (typeof x == "undefined") obj[field] = []
+    else obj[field] = [x]
 }
 
 async function processTask(fname) {
@@ -61,11 +95,10 @@ async function processTask(fname) {
     
     await exec("cp", [wasm_file, tmp_dir + "/" + wasm_file], process.cwd())
     
-    argv.arg = argv.arg || []
-    argv.file = argv.file || []
+    console.log(argv)
     
-    if (typeof argv.file == "string") argv.file = [argv.file]
-    if (typeof argv.arg == "string") argv.arg = [argv.arg]
+    clean(argv, "arg")
+    clean(argv, "file")
 
     console.log(argv)
     for (var i = 0; i < argv.file.length; i++) {
@@ -77,10 +110,22 @@ async function processTask(fname) {
     await exec(wasm, ["-underscore", wasm_file])
     await exec(wasm, ["-merge", "underscore.wasm", dir + "filesystem.wasm"])
     await exec(wasm, ["-add-globals", "globals.json", "merge.wasm"])
-    var mem_size = argv["memory-size"] || "25"
+
     var args = flatten(argv.arg.map(a => ["-arg", a]))
     args = args.concat(flatten(argv.file.map(a => ["-file", a])))
-    await exec(wasm, ["-m", "-input", "-file", "record.bin", "-table-size", "20", "-stack-size", "20", "-memory-size", mem_size, "-wasm", "globals.wasm"].concat(args))
+    var result_wasm = "globals.wasm"
+    var float_memory = 10*1024
+
+    if (argv.float) {
+        await exec(wasm, ["-shift-mem", float_memory, "globals.wasm"])
+        await exec(wasm, ["-memory-offset", float_memory, "-int-float", dir + "softfloat.wasm", "shiftmem.wasm"])
+        result_wasm = "intfloat.wasm"
+        args.push("-memory-offset")
+        args.push(float_memory)
+    }
+
+    var mem_size = argv["memory-size"] || "25"
+    await spawnPromise(wasm, ["-m", "-input", "-file", "record.bin", "-table-size", "20", "-stack-size", "20", "-memory-size", mem_size, "-wasm", result_wasm].concat(args))
     var hash = await uploadIPFS("globals.wasm")
     console.log("Uploaded to IPFS ", hash)
 }
